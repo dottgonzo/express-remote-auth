bb4138b6b05cbe0077e1eb07708af3db044a8554
dario <darioyzf@gmail.com>
2016-08-10 11:40:05 +0200

    starting now

---
+++ index.ts
@@ -0,0 +1,28 @@
+import aut from "remote-auth";
+
+import * as express from "express";
+
+export default function (o: { secret: string, app_id: string, couchdb: string }) {
+
+    const authmodule = new aut({ secret: o.secret, app_id: o.app_id, couchdb: o.couchdb });
+
+    const Router = express.Router();
+
+
+    Router.get("/authorize/:provider/:user/:password", (req, res) => {
+
+        const params = req.params;
+
+        authmodule.authorize({ provider: params.provider, user: params.user, password: params.password }).then((a) => {
+            res.send(a)
+        }).catch((err) => {
+            console.error(err)
+        })
+
+
+    })
+
+
+}
+
+
